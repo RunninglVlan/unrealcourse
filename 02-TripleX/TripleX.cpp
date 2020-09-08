9d2402fd9e36fc28e0c8ad8d6bbd1a849eff9601
RunninglVlan <RedCapShortCut@gmail.com>
2020-09-08 16:35:17 +0300

    Completed 17. Hello, World!

--- 02-TripleX/TripleX.cpp
+++ 02-TripleX/TripleX.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+    std::cout << "Enter the correct code in the form of `X X X` (where X is a number) to continue..." << std::endl;
+
     return 0;
 }
