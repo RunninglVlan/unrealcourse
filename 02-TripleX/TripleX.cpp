7693f1ec254e1635836cc420451a4675f4852961
RunninglVlan <RedCapShortCut@gmail.com>
2020-09-08 16:47:14 +0300

    Completed 22. Naming & Self Documenting Code, added local fmt

--- 02-TripleX/TripleX.cpp
+++ 02-TripleX/TripleX.cpp
@@ -1,9 +1,19 @@
+#define FMT_HEADER_ONLY
+
 #include <iostream>
+#include "fmt/format.h"
 
 int main()
 {
     std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
     std::cout << "Enter the correct code in the form of `X X X` (where X is a number) to continue..." << std::endl;
+
+    const int CodeA = 2, CodeB = 3, CodeC = 4;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << fmt::format("+ The codes add-up to: {}", CodeSum) << std::endl;
+    std::cout << fmt::format("+ The codes multiply to give: {}", CodeProduct) << std::endl;
 
     return 0;
 }
